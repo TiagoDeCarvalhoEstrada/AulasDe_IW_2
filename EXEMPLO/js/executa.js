9c78ebf6df2adea931e09eb4a49028b016335579
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-02-29 16:56:59 -0300

    Delete EXEMPLO directory

--- EXEMPLO/js/executa.js
+++
@@ -1,8 +0,0 @@
-var resultado = document.getElementById('resultado')
-
-
-function pegaNome(){
-    var nome = document.getElementById('nome').value
-    //console.log('nome: ',nome)
-    resultado.innerHTML = nome
-}
