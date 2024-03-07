785b2826b5ac393a40660bbcb1172c6b9b81ff33
TiagoDeCarvalhoEstrada <etcarvalhoe@gmail.com>
2024-03-07 17:45:52 -0300

    FINALY

--- Aula 4/IMC-calc/js/script.js
+++
@@ -1,6 +0,0 @@
-function opera(){
-    var num1 = number(document.getElementById('num1').value)
-    var num2 = number(document.getElementById('num2').value)
-    var calc = num2 / (num1 * num1)
-    resultado.innerHTML = calc
-}
---
+++ aula_03/EXEMPLO/js/executa.js
@@ -0,0 +1,8 @@
+var resultado = document.getElementById('resultado')
+
+
+function pegaNome(){
+    var nome = document.getElementById('nome').value
+    //console.log('nome: ',nome)
+    resultado.innerHTML = nome
+}
